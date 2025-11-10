008d0e4a58616528f853021f3246809414211c1e
winwave <letrannhaclong@gmail.com>
2025-11-10 14:43:59 +0700

    problem2: config tailwind and packages

--- src/problem2/eslint.config.js
+++ src/problem2/eslint.config.js
@@ -1,23 +1,47 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+// @ts-check
 
-export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+import eslintPluginReact from 'eslint-plugin-react';
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
+import eslintPluginJsxA11y from 'eslint-plugin-jsx-a11y';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+    ...tseslint.configs.recommended,
+
+    {
+      files: ['src/**/*.{js,jsx,ts,tsx}'],
+      plugins: {
+        'react': eslintPluginReact,
+        'react-hooks': eslintPluginReactHooks,
+        'jsx-a11y': eslintPluginJsxA11y,
+      },
+      rules: {
+        ...eslintPluginReact.configs.recommended.rules,
+        ...eslintPluginReactHooks.configs.recommended.rules,
+        ...eslintPluginJsxA11y.configs.recommended.rules,
+
+        'react/react-in-jsx-scope': 'off',
+      },
+      languageOptions: {
+        globals: {
+          browser: true,
+        },
+      },
+      settings: {
+        react: {
+          version: 'detect',
+        },
+      },
     },
-  },
-])
+
+    {
+      ignores: [
+        'dist',
+        'node_modules',
+        'vite.config.ts',
+        'postcss.config.js',
+        'tailwind.config.js',
+        'eslint.config.js',
+      ],
+    }
+);
